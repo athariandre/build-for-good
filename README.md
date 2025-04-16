35e28958f0204cd8706ece18e4dea7cc2612fbab
athariandre <athariandre@gmail.com>
2025-04-15 23:37:11 -0500

    hi

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# build-for-good
-This is our submission for the build-for-good hackathon.
+# HOW TO RUN:
+python3 debug.py
+OR
+python debug.py
+
+depending on your python installation
--- frontend/renderer.js
+++
@@ -1,5 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-document.getElementById('goToSecondScreen').addEventListener('click', () => {
-  ipcRenderer.send('change-window-content', './screens/second.html');
-});
